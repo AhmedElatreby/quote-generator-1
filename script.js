f63547c5af03ebb6066b7a24bcb61cf75491ff33
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-14 15:29:55 +0100

    Render a new quote on page load and on click of new quote button

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 import localQuotes from "./localQuotes.js"
 let fetchedQuotes
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
 
-function newQuote(quotes) {
+newQuoteBtn.addEventListener("click", getQuotes)
+
+function renderQuote(quotes) {
   const quote = quotes[Math.floor(Math.random() * quotes.length)]
-  console.log(quote)
+  
+  if (!quote.author) {
+    authorText.textContent = "Unknown"
+  } else {
+    authorText.textContent = quote.author
+  }
+
+  if (quote.text.length > 50) {
+    quoteText.className = "long-quote"
+  } else {
+    quoteText.className = ""
+  }
+  quoteText.textContent = quote.text
 }
 
 async function getQuotes() {
-  const apiUrl = "https://type.fit/api/quotes"  
-  
+  const apiUrl = "https://type.fit/api/quotes"
+
   try {
     const response = await fetch(apiUrl)
     fetchedQuotes = await response.json()
-    
-    if(fetchedQuotes) {
-      newQuote(fetchedQuotes)
+
+    if (fetchedQuotes) {
+      renderQuote(fetchedQuotes)
     }
   } catch (error) {
     console.log(error)
-    newQuote(localQuotes)
+    renderQuote(localQuotes)
   }
 }
 
